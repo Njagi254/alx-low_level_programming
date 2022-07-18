7dbe3ec86f48e7d6428e2d087542e43130fac33e
Njagi254 <samuelkamau09@gmail.com>
2022-07-18 14:10:43 +0000

    syntax edit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  *main - Entry point
- *Description : 'assign a number to the variable n to print on execution'
+ *Description : assign a number to the variable n to print on execution
  *Return : always 0 (Success)
 */
 int main(void)
@@ -12,7 +12,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	printf("%d is positive\n", n);
 	if (n == 0)
