e4428d3f523c18a0669b385410c72bfe79d114f4
Njagi254 <samuelkamau09@gmail.com>
2022-07-20 23:55:10 +0000

    commit file for task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:'prints all single digit numbers of base 10 starting
+ * from 0, followed by a new line.'
+ *
+ * Return : Always 0(Success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -3,3 +3,4 @@ readme file for task 1
 readme file for task 2
 readme file for task 3
 readme file for task 4
+readme file for task 5
