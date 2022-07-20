c341c9b1b3344ecfe23acc72b6291abd2640e55e
Njagi254 <samuelkamau09@gmail.com>
2022-07-20 20:16:48 +0000

    commit file for 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *program that prints the alphabet
+ *in lowercase, and then in uppercase, followed by a new line
+ *Return : always 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 readme file for task 0
 readme file for task 1
 readme file for task 2
+readme file for task 3
