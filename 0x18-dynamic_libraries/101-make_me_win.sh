a631ed4f486a60dc22e7daac766035e0e4f1dbef
Njagi254 <samuelkamau09@gmail.com>
2023-01-18 06:12:57 +0300

    corrections

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Njagi254/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/Njagi254/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
--- 0x18-dynamic_libraries/2-main.c
+++ 0x18-dynamic_libraries/2-main.c
@@ -4,8 +4,7 @@
 
 int printf(const char *format, ...)
 {
-	write(1, "9 8 10 24 75 - 9\n", 17);
-	write (1, "Congratulations, you win the Jackpot!", 37);
+	write(1, "9 8 10 24 75 9\n", 17);
+	write (1, "Congratulations, you win the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
 }
-
